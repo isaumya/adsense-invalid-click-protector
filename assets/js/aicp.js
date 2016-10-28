2a50a2fb585a9948ebe9d3d7cb99dd7924359451
Saumya Majumder <saumya0305@gmail.com>
2016-10-29 02:38:55 +0530

    Updated the JS file with addition of proper check before fireing the codes and also adding display none if the user pass the click bombing limit

--- assets/js/aicp.js
+++ assets/js/aicp.js
@@ -1,22 +1,27 @@
 (function($){
-	var click = 0;
-	$(".aicp").click(function(){
-		++count; //checking how many times uses click on the ads
-		//if the user click on ads for more than 3 times
-		if( count > 3 ) {
-			// Now it's AJAX time to handle the data and push it to database
-			jQuery.ajax({
-				type: 'POST',
-				url: AICP.ajaxurl,
-				data: {
-					"action": "process_data", 
-					"nonce": AICP.nonce,
-					"ip": AICP.ip,
-					"countryName": AICP.countryName,
-					"countryCode": AICP.countryCode,
-					"click_count": count
-				},
-			});
-		}
-	});
+	// Fire the code only of any ad codes exists
+	if ( $(".aicp").length > 0 ) {
+		var click = 0;
+		$(".aicp").click(function(){
+			++count; //checking how many times uses click on the ads
+			//if the user click on ads for more than 3 times
+			if( count > 3 ) {
+				// If the visitor is click bombing, stop showing ads immidiately.
+				$(".aicp").css({ display: "none" });
+				// Now it's AJAX time to handle the data and push it to database
+				jQuery.ajax({
+					type: 'POST',
+					url: AICP.ajaxurl,
+					data: {
+						"action": "process_data", 
+						"nonce": AICP.nonce,
+						"ip": AICP.ip,
+						"countryName": AICP.countryName,
+						"countryCode": AICP.countryCode,
+						"click_count": count
+					},
+				});
+			}
+		});
+	}
 })(jQuery);
